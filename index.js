167925ea9c1da25e44d0b3e5eb1c0ec15c22839f
Jackie <zhengjiekun@gmail.com>
2019-11-20 18:07:43 +0800

    hot fix on error 601

--- index.js
+++ index.js
@@ -1,61 +1,64 @@
-import { NativeModules } from 'react-native';
-import Q from 'q'
+import { NativeModules } from "react-native";
+import Q from "q";
 
 const { RNSslPinning } = NativeModules;
 
 const fetch = (url, obj, callback) => {
-    let deferred = Q.defer();
-    RNSslPinning.fetch(url, obj, (err, res) => {
-        if (err && typeof err != 'object') {
-            deferred.reject(err);
-        }
-
-        let data = err || res;
-
-        data.json = function() {
-            return Q.fcall(function() {
-                return JSON.parse(data.bodyString);
-            });
-        };
-
-        data.text = function() {
-            return Q.fcall(function() {
-                return data.bodyString;
-            });
-        };
-
-        data.url = url;
-
-        if (err) {
-            deferred.reject(data);
-        } else {
-            deferred.resolve(data);
-        }
-
-        deferred.promise.nodeify(callback);
-    });
-
-    return deferred.promise;
-};
-
-const getCookies = (domain) => {
-    if(domain) {
-        return RNSslPinning.getCookies(domain);
+  let deferred = Q.defer();
+  RNSslPinning.fetch(url, obj, (err, res) => {
+    if (err && typeof err != "object") {
+      deferred.reject(err);
     }
 
-    return Promise.reject("Domain cannot be empty")
-};
+    let data = err || res;
 
-const removeCookieByName = (name) => {
-    if(name) {
-        return RNSslPinning.removeCookieByName(name);
+    if (data) {
+      data.json = function() {
+        return Q.fcall(function() {
+          return JSON.parse(data.bodyString);
+        });
+      };
+
+      data.text = function() {
+        return Q.fcall(function() {
+          return data.bodyString;
+        });
+      };
+
+      data.url = url;
+
+      if (err) {
+        deferred.reject(data);
+      } else {
+        deferred.resolve(data);
+      }
+    } else {
+      deferred.reject({
+        status: 601,
+        url: url
+      });
     }
 
-    return Promise.reject("Cookie Name cannot be empty")
+    deferred.promise.nodeify(callback);
+  });
+
+  return deferred.promise;
 };
 
-export {
-    fetch,
-    getCookies,
-    removeCookieByName
-}
+const getCookies = domain => {
+  if (domain) {
+    return RNSslPinning.getCookies(domain);
+  }
+
+  return Promise.reject("Domain cannot be empty");
+};
+
+const removeCookieByName = name => {
+  if (name) {
+    return RNSslPinning.removeCookieByName(name);
+  }
+
+  return Promise.reject("Cookie Name cannot be empty");
+};
+
+export { fetch, getCookies, removeCookieByName };
